566ccf6790626b13557bd0813f8e1f531983f1ee
dsorcererdev <dsorcererdev@yahoo.com>
2022-08-04 12:01:35 +0200

    Fix project structure, remove seconds from counter

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,10 @@ let clock = () => {
   let now = new Date();
 	let hours = now.getHours();
 	let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
   let time = 0;
 	
-  if (minutes < 10) time = `${hours}:0${minutes}:${seconds}`;
-	else time = `${hours}:${minutes}:${seconds}`;
+  if (minutes < 10) time = `${hours}:0${minutes}`;
+	else time = `${hours}:${minutes}`;
 	
   if (hours < 10) time = `0${time}`;
   
